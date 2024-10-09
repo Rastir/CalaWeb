14ea27cf8c53cda5ab2909d70bc09f3270e0982b
Rastir <aldojero@hotmail.com>
2024-10-09 17:06:56 -0500

    added search page

--- js/api.js
+++ js/api.js
@@ -1,18 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const apiUrl = 'http://vps-4437505-x.dattaweb.com:8080/personas/traer'; // Reemplaza con la URL de tu API
+// url frl spi
+const apiUrl ="http://vps-4437505-x.dattaweb.com:8080/personas/traer"
 
-    fetch(apiUrl)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error en la solicitud');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const apiDataDiv = document.getElementById('api-data');
-            apiDataDiv.innerHTML = JSON.stringify(data, null, 2);
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
-});
+fetch(apiUrl)
+.then(Response => Response.json())
+.then(data => console.log(data))
+.catch(error => console.error(error))
